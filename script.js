7fa42f259c36acd16e077a9e67b07efe5e5c1ea1
guilhermeulbriki <guiulbriki@gmail.com>
2020-04-17 19:40:09 -0300

    x1 dezena e corona

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 var infecteds;
 
 jQuery.ajax({
-    type: 'GET',
-    url: 'https://api.apify.com/v2/key-value-stores/TyToNta7jGKkpszMZ/records/LATEST?disableRedirect=true',
-    success: function (data) {
+  type: 'GET',
+  url: 'https://api.apify.com/v2/key-value-stores/TyToNta7jGKkpszMZ/records/LATEST?disableRedirect=true',
+  success: function (data) {
 
-        var result = JSON.parse(JSON.stringify(data));
+    var result = JSON.parse(JSON.stringify(data));
 
-        jQuery("#qtd").text(result.infected);
-        console.log(infecteds);
-    }
+    jQuery("#qtd-corona").text(result.infected);
+  }
+});
+
+var dezenas;
+
+jQuery.ajax({
+  type: 'GET',
+  url: 'https://corona-nsg-jumasbrasil.herokuapp.com/dezenas',
+  success: function (data) {
+
+    var result = JSON.parse(JSON.stringify(data));
+
+    jQuery("#qtd-dezenas").text(result.quantidade);
+  }
 });

e83bb0062198b0dd1d7d914921866aaa61d3e6fa
ardsantos <arsbetinho@gmail.com>
2020-04-17 03:03:18 -0300

    Formulário de dezenas adicionado

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-var infecteds;
-
 jQuery.ajax({
     type: 'GET',
     url: 'https://api.apify.com/v2/key-value-stores/TyToNta7jGKkpszMZ/records/LATEST?disableRedirect=true',
     success: function (data) {
 
-        var result = JSON.parse(JSON.stringify(data));
+        const result = JSON.parse(JSON.stringify(data));
 
         jQuery("#qtd").text(result.infected);
-        console.log(infecteds);
     }
 });
+
+function submitDezenas() {
+  const inputField = document.querySelector('#inputDezenas');
+  const dezenas = inputField.value;
+  const feedback = document.querySelector('#feedback');
+  if (dezenas < 1 || dezenas > 20) {
+    inputField.style.borderColor = "#e13c39";
+    feedback.textContent = "O número de dezenas deve estar entre 1 e 20.";
+    feedback.style.color = '#e13c39';
+  } else {
+    inputField.style.borderColor = "#cecece";
+    inputField.value = '';
+    jQuery.ajax({
+      type: 'POST',
+      url: 'https://corona-nsg-jumasbrasil.herokuapp.com/registrar',
+      data: {quantidade: dezenas},
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (data) {
+        console.log(data['responseJSON']['error']);
+      }
+  });
+    feedback.textContent = "Dezenas registradas com sucesso!";
+    feedback.style.color = '#008000';
+    console.log(dezenas);
+  }
+  
+}
